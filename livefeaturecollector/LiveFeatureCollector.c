25c0ed48483bcd14c3f86aea6ef54f6ff8288b72
SysBot <c.lahouazi@esi-sba.dz>
2020-05-10 05:46:39 +0100

    add: define the structure of tcp and udp packets.

--- livefeaturecollector/LiveFeatureCollector.c
+++ livefeaturecollector/LiveFeatureCollector.c
@@ -54,6 +54,39 @@ struct ip_header{
 	u_int16_t	ip_sum;		/* checksum */
 	struct	in_addr ip_src,ip_dst;	/* source and dest address */
 };
+/* TCP header */
+
+typedef u_int tcp_seq;
+struct tcp_header{
+    u_short th_sport;	/* source port */
+    u_short th_dport;	/* destination port */
+    tcp_seq th_seq;		/* sequence number */
+    tcp_seq th_ack;		/* acknowledgement number */
+    u_char th_offx2;	/* data offset, rsvd */
+	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+    u_char th_flags;
+	#define TH_FIN 0x01
+	#define TH_SYN 0x02
+	#define TH_RST 0x04
+	#define TH_PUSH 0x08
+	#define TH_ACK 0x10
+	#define TH_URG 0x20
+	#define TH_ECE 0x40
+	#define TH_CWR 0x80
+	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short th_win;		/* window */
+    u_short th_sum;		/* checksum */
+    u_short th_urp;		/* urgent pointer */
+};
+
+/* UDP header*/
+struct udp_header{
+    u_short	uh_sport;		/* source port */
+	u_short	uh_dport;		/* destination port */
+	u_short	uh_ulen;		/* datagram length */
+	u_short	uh_sum;			/* datagram checksum */
+};
+
 
 /* looking at ethernet headers */
 void Jacket(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet){
@@ -124,6 +157,24 @@ u_char* handle_IP(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* pa
 
     return NULL;
 }
+
+/**
+ * 
+ * handle tcp packets
+ * 
+*/
+
+u_char* handle_TCP(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet){
+
+}
+
+
+/**
+ * handle udp packets
+*/
+ u_char* handle_UDP(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet){
+     
+ }
 
 /* handle ethernet packets when captured.
  */
